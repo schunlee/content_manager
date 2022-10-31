9d70ebefa5be45c5e3b26c5df8152f2dc6811115
bill.li <bill@nexa-corp.com>
2022-10-31 21:26:46 +0800

    update models.

--- backend/manager/models.py
+++ backend/manager/models.py
@@ -53,20 +53,56 @@ class Customer(models.Model):
     single_flag = BooleanField(default=False, verbose_name="是否去重")
     create_time = TimeField(auto_now_add=True, verbose_name="商户新增时间")
     modified_time = TimeField(auto_now=True, verbose_name="商户信息修改时间")
-    group = ForeignKey(to=BusinessGroup, on_delete=models.CASCADE, verbose_name="分组", related_name='groups')  # 分组
+    group = ForeignKey(BusinessGroup, on_delete=models.CASCADE, verbose_name="分组", related_name='customers')  # 分组
 
 
 class ClerkType(models.Model):
     '''
     客服类型
     通过django admin配置
+
+    zalo, line, fb-messenger, whatsapp, lulu-zalo, lulu-fb-messenger, lulu-line, lulu-whatsapp, 外部链接
     '''
     name = CharField(max_length=50, blank=False, null=False, verbose_name="客服类型")
+
+    class Meta:
+        ordering = ('name',)
 
 
 class Clerk(models.Model):
     '''
     客服
     '''
-    name = CharField(max_length=50, blank=False, null=False, verbose_name="名称")
+    clerk_number = CharField(max_length=50, blank=False, null=False, verbose_name="客服号码")
     note = TextField(blank=False, null=False, verbose_name="备注")
+    clerk_type = ForeignKey(ClerkType, on_delete=models.CASCADE, verbose_name="类型", related_name="clerks")
+
+
+class Country(models.Model):
+    '''
+    国家
+    通过django admin配置
+    '''
+    name = CharField(max_length=100, null=False, blank=False, verbose_name="国家名称")
+
+
+class App(models.Model):
+    '''
+    App
+    '''
+    BEHAVIOR_IN_APP_CHOICES = [
+        ("inner", "内跳"),
+        ("outer", "外跳")
+    ]
+    STATUS_IN_APP_CHOICES = [
+        ("test", "待测试"),
+        ("stop", "已停用"),
+        ("develop", "开发中"),
+        ("review", "提审中"),
+        ("live", "在线")
+    ]
+    name = CharField(max_length=50, verbose_name="应用名称")
+    behavior = CharField(max_length=10, choices=BEHAVIOR_IN_APP_CHOICES, default="outer", verbose_name="跳页方式")
+    note = TextField(null=True, blank=True, verbose_name="备注")
+    target_country = ForeignKey(Country, on_delete=True, verbose_name="主推国家")
+    status = CharField(max_length=50, choices=STATUS_IN_APP_CHOICES, verbose_name="状态")
